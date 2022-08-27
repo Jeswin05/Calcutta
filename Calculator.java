312942f04039a0c92c641259b17d343f2fd7e324
Jeswin <jeswinjomi05@gmail.com>
2022-08-27 16:32:35 +0530

    JLabel Added

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,31 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 public class Calculator {
-    public Calculator(){
-        JFrame jf= new JFrame("Calculator");
-        jf.setSize(500,600);
-        jf.setLayout(null);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    public static void main(String[] args) {
-        new Calculator();
-    }
+
+  public Calculator() {
+    Color bgcolor = Color.decode("#54BAB9");
+    Color labelcolor =Color.decode("#9ED2C6");
+
+    JFrame frame = new JFrame("Calculator");
+    frame.setSize(500, 600);
+    frame.setLayout(null);
+
+    JLabel displayLabel = new JLabel();
+    displayLabel.setBounds(30, 50, 440, 80);
+    displayLabel.setBackground(labelcolor);
+    displayLabel.setOpaque(true);
+
+
+    
+    frame.add(displayLabel);
+    frame.setVisible(true);
+    frame.getContentPane().setBackground(bgcolor);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public static void main(String[] args) {
+    new Calculator();
+  }
 }
